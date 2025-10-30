dd68c776ae882a85c45eee09b721d927b326be30
David Parsley <davidparsley.kakhayanga@gmail.com>
2025-10-30 20:25:05 +0300

    Transferred from main account

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const input = document.getElementById('button');
 function addingEventListener() {
-    alert('I was clicked!')
     input.addEventListener('click', addingEventListener)
+     return alert('I was clicked!')
 }
